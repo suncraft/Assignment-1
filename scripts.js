2dcf3731a1caa86c4620088a72692928e22cb0f7
suncraft <62112694+suncraft@users.noreply.github.com>
2020-10-09 10:45:26 -0700

    Attempt 1 JS
    not quite working

---
+++ scripts.js
@@ -0,0 +1,67 @@
+
+function mOver(obj) {
+  // alert();
+  document.getElementById("description").style.display = "block";
+}
+
+function mOut(obj) {
+  // alert();
+  document.getElementById("description").style.display = "none";
+}
+
+
+//
+//
+// var x = document.getElementById("card");
+// x.addEventListener("mouseover", myFunction);
+// x.addEventListener("mouseout", mySecondFunction);
+//
+// function myFunction() {
+//   document.getElementById("hide").style.display = "block";
+// }
+//
+// function mySecondFunction() {
+//   document.getElementById("demo").style.display = "none";
+// }
+
+
+
+// var myElement = document.getElementById("preview1");
+// myElement.style.display = "none";
+
+// function mOver(obj) {
+//   document.getElementById("description").style.display = "block";
+//
+// }
+//
+// function mOut(obj) {
+//   document.getElementById("description").style.display = "none";
+// }
+//
+//
+//
+// function mOver(obj) {
+//   document.getElementById("preview1").style.display = "block";
+// }
+//
+// function mOver(obj) {
+//   document.getElementById("#preview2").style.display = "block";
+// }
+//
+// function mOver(obj) {
+//   document.getElementById("#preview3").style.display = "block";
+// }
+//
+// function mOver(obj) {
+//   document.getElementById("#preview4").style.display = "block";
+// }
+//
+// function mOver(obj) {
+//   document.getElementById("#preview5").style.display = "block";
+// }
+//
+//
+//
+// function mOut(obj) {
+//   document.getElementById("#preview1, #preview2, #preview3, #preview4, #preview5 ").style.display = "none";
+}
